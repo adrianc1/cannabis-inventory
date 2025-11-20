a85f296332941483116ee5e04ce405d2a264b7c0
adriansito <adriancrosby02@gmail.com>
2025-11-19 18:47:47 -0800

    first commit: set up project and dependencies

---
+++ db/pool.js
@@ -0,0 +1,5 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool({
+	connectionString: `postgresql://${process.env.USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+});

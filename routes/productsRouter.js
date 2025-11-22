556613f3be70c0323b0149ef1ac6d0949ef3e34a
adriansito <adriancrosby02@gmail.com>
2025-11-21 16:08:52 -0800

    added homepage linking to all products page. added back to all products from single product page

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const productsController = require('../controllers/productsController');
 const router = express.Router();
 
-router.get('/all', productsController.getAllProducts);
+router.get('/', productsController.getAllProducts);
 router.get('/:id', productsController.getProduct);
 module.exports = router;

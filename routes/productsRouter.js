4311051f4c49a1e70c1ee531d67f1dede69b64d1
adriansito <adriancrosby02@gmail.com>
2025-12-02 20:21:15 -0800

    created new product form with query to db for list of brands

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,5 +3,7 @@ const productsController = require('../controllers/productsController');
 const router = express.Router();
 
 router.get('/', productsController.getAllProducts);
+router.get('/create-product', productsController.createProduct);
 router.get('/:id', productsController.getProduct);
+
 module.exports = router;

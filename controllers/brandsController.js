c87b3e95baf86f1a52a5a26527aaa93af1ac22fe
adriansito <adriancrosby02@gmail.com>
2025-12-14 20:47:43 -0800

    created all brands route

--- controllers/brandsController.js
+++ controllers/brandsController.js
@@ -2,7 +2,7 @@ const db = require('../db/queries');
 
 const getAllBrands = async (req, res) => {
 	const brands = await db.getAllBrands();
-	res.render('allBrands', { message: 'brands!', brands });
+	res.render('brands/allBrands', { message: 'brands!', brands });
 };
 
 module.exports = { getAllBrands };

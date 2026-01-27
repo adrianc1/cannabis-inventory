d616dc0c44a342a15035015dfffde7bcce741ca2
adriansito <adriancrosby02@gmail.com>
2026-01-26 19:27:35 -0800

    set up configuration for authentication and install bcryptjs for hasing pw

--- auth/passport.js
+++ auth/passport.js
@@ -1,41 +1,46 @@
-const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const db = require('../db/queries');
+const pool = require('../db/pool');
 
-passport.use(
-	new LocalStrategy(async (username, password, done) => {
+module.exports = function (passport) {
+	passport.use(
+		new LocalStrategy(async (username, password, done) => {
+			try {
+				const user = await db.getUserByUsername(username);
+
+				if (!user) {
+					return done(null, false, { message: 'Incorrect username' });
+				}
+				const match = await bcrypt.compare(password, user.password_hash);
+
+				if (!match) {
+					console.log('it failed');
+					return done(null, false, { message: 'Incorrect password' });
+				}
+				console.log('it worked!!');
+
+				return done(null, user);
+			} catch (err) {
+				return done(err);
+			}
+		}),
+	);
+
+	passport.serializeUser((user, done) => {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(async (id, done) => {
 		try {
-			const { rows } = await pool.query(
-				'SELECT * FROM users WHERE username = $1',
-				[username],
-			);
+			const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
+				id,
+			]);
 			const user = rows[0];
 
-			if (!user) {
-				return done(null, false, { message: 'Incorrect username' });
-			}
-			if (user.password !== password) {
-				return done(null, false, { message: 'Incorrect password' });
-			}
-			return done(null, user);
+			done(null, user);
 		} catch (err) {
-			return done(err);
+			done(err);
 		}
-	}),
-);
-
-passport.serializeUser((user, done) => {
-	done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-	try {
-		const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
-			id,
-		]);
-		const user = rows[0];
-
-		done(null, user);
-	} catch (err) {
-		done(err);
-	}
-});
+	});
+};

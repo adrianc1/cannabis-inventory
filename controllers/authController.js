9c5bb4875fadcac59668cba95e3c60cc86c5530e
adriansito <adriancrosby02@gmail.com>
2026-01-28 21:17:38 -0800

    hashed and salt password input from signup form

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,14 @@
 const db = require('../db/queries');
+const bcrypt = require('bcryptjs');
 
 const getSignUpForm = async (req, res) => {
 	res.render('auth/signup.ejs');
 };
 
 const postSignUpForm = async (req, res) => {
-	const { firstName, lastName, email, password_hash, company, license } =
-		req.body;
+	const { firstName, lastName, email, company, license } = req.body;
+	console.log('the body!!!====', req.body);
+	const password_hash = await bcrypt.hash(req.body.password, 10);
 	const products = await db.getAllProductsDB();
 	await db.signupAdmin(
 		firstName,

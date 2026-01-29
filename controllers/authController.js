719684d4d2638dbd3b05fa2c43aa91896d14a730
adriansito <adriancrosby02@gmail.com>
2026-01-28 21:38:55 -0800

    installed and implemented express validator to validate signup forms

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,49 @@
 const db = require('../db/queries');
 const bcrypt = require('bcryptjs');
+const { body, validationResult, matchedData } = require('express-validator');
+
+const validateUser = [
+	body('firstName')
+		.trim()
+		.isAlpha()
+		.withMessage(`First name ${alphaErr}`)
+		.isLength({ min: 2, max: 10 })
+		.withMessage(`First name ${lengthErr}`),
+	body('lastName')
+		.trim()
+		.isAlpha()
+		.withMessage(`Last name ${alphaErr}`)
+		.isLength({ min: 2, max: 10 })
+		.withMessage(`Last name ${lengthErr}`),
+	body('email')
+		.trim()
+		.isEmail()
+		.withMessage(`Must enter a valid email`)
+		.isLength({ min: 1 })
+		.withMessage(`Email must at least contain 1 character `),
+	body('company')
+		.isLength({ min: 1 })
+		.withMessage(`company must at least contain 1 character `),
+	,
+	body('password')
+		.isLength({ min: 4 })
+		.withMessage('password must be at least 8 characters'),
+	body('confirmPassword')
+		.custom((value, { req }) => {
+			return value === req.body.password;
+		})
+		.withMessage('passwords do not match!'),
+];
 
 const getSignUpForm = async (req, res) => {
 	res.render('auth/signup.ejs');
 };
 
 const postSignUpForm = async (req, res) => {
-	const { firstName, lastName, email, company, license } = req.body;
+	const error = validationResult(req);
+	const { firstName, lastName, email, company, license } = matchedData(
+		req.body,
+	);
 	console.log('the body!!!====', req.body);
 	const password_hash = await bcrypt.hash(req.body.password, 10);
 	const products = await db.getAllProductsDB();
@@ -32,4 +69,5 @@ module.exports = {
 	getSignUpForm,
 	postSignUpForm,
 	getLoginForm,
+	validateUser,
 };
